8dd17244f685fbbf42b52d0583fc2e283d0c9492
Raknage <toni0haila@gmail.com>
2022-11-27 22:41:22 +0200

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, n) {
+  if (n < 0) {
+    return "ERROR";
+  }
+  let newStr = "";
+  for (let i = 0; i < n; i++) {
+    newStr += str;
+  }
+  return newStr;
 };
 
 // Do not edit below this line
