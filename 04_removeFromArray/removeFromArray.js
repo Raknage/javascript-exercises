7538eecfbfe624c05a2bc62b7bcb85603bd3ebaa
Raknage <toni0haila@gmail.com>
2022-11-28 18:45:58 +0200

    Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (a, ...args) {
+  let newArray = a;
+  let index;
+  for (let arg in args) {
+    index = newArray.findIndex((e) => e === args[arg]);
+    if (index != -1) {
+      newArray.splice(index, 1);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
