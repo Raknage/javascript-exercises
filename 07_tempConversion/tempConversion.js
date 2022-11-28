abc0008688c38b2d2537f26185d5212e65d6f24e
Raknage <toni0haila@gmail.com>
2022-11-28 21:07:20 +0200

    Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (n) {
+  return +((n - 32) / 1.8).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (n) {
+  return +(n * 1.8 + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
