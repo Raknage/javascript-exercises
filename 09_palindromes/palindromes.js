379028d1baee13e7b91797cb75caa66bb6ef62cf
Raknage <toni0haila@gmail.com>
2023-01-17 23:14:24 +0200

    09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const array = str
+    .toLowerCase()
+    .split("")
+    .filter((e) => e <= "z" && e >= "a");
 
+  const str1 = array.join("");
+  const str2 = array.reverse().join("");
+
+  return str1 === str2;
 };
 
 // Do not edit below this line
