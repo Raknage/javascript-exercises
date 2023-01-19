169fd942c20ef673dafab550006a1733179e520e
Raknage <toni0haila@gmail.com>
2023-01-19 16:48:46 +0200

    12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const year = new Date().getFullYear();
+  return array.reduce((oldest, person) => {
+    const oldestAge = () => {
+      if (!oldest.yearOfDeath) oldest.yearOfDeath = year;
+      return oldest.yearOfDeath - oldest.yearOfBirth;
+    };
+    const personAge = () => {
+      if (!person.yearOfDeath) person.yearOfDeath = year;
+      return person.yearOfDeath - person.yearOfBirth;
+    };
 
+    if (oldestAge() < personAge()) {
+      oldest = person;
+    }
+
+    return oldest;
+  });
 };
 
 // Do not edit below this line
