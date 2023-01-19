eeb043aeac36496d873d1da472eb6f12349069d1
Raknage <toni0haila@gmail.com>
2023-01-19 14:52:38 +0200

    10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n <= 0) return "OOPS";
+  let x = 1;
+  let y = 1;
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    x = y;
+    y = result;
+    result = x + y;
+  }
+  return result;
 };
 
 // Do not edit below this line
